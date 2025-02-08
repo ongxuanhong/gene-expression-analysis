a4233d49dc80b423039ab9453632a5c86ba1ca8a
hongong <hongong>
2025-02-08 08:48:11 +0700

    Update trimming commands

--- project_01/02.trim.sh
+++ project_01/02.trim.sh
@@ -1,4 +1,6 @@
 # conda install -c bioconda trimmomatic -y
+# mkdir -p adapters
+# wget https://raw.githubusercontent.com/timflutre/trimmomatic/master/adapters/TruSeq3-PE.fa -O adapters/TruSeq3-PE.fa
 # Create directory for trimmed files
 mkdir trimmed_reads
 
@@ -7,7 +9,7 @@ trimmomatic PE \
   Tien_Cont1_S25_R1_001.fastq.gz Tien_Cont1_S25_R2_001.fastq.gz \
   trimmed_reads/Cont1_R1_paired.fastq.gz trimmed_reads/Cont1_R1_unpaired.fastq.gz \
   trimmed_reads/Cont1_R2_paired.fastq.gz trimmed_reads/Cont1_R2_unpaired.fastq.gz \
-  ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+  ILLUMINACLIP:adapters/TruSeq3-PE.fa:2:30:10 \
   LEADING:3 TRAILING:3 \
   SLIDINGWINDOW:4:15 \
   MINLEN:36
@@ -17,7 +19,7 @@ trimmomatic PE \
   Tien_Zn2_S29_R1_001.fastq.gz Tien_Zn2_S29_R2_001.fastq.gz \
   trimmed_reads/Zn2_R1_paired.fastq.gz trimmed_reads/Zn2_R1_unpaired.fastq.gz \
   trimmed_reads/Zn2_R2_paired.fastq.gz trimmed_reads/Zn2_R2_unpaired.fastq.gz \
-  ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 \
+  ILLUMINACLIP:adapters/TruSeq3-PE.fa:2:30:10 \
   LEADING:3 TRAILING:3 \
   SLIDINGWINDOW:4:15 \
   MINLEN:36
