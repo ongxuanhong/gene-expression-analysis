75f7cc27c9554763deabe6dace56a0f8f3682764
hongong <hongong>
2025-02-11 05:57:07 +0700

    Update list download sample

--- project_01/00.download.sh
+++ project_01/00.download.sh
@@ -2,7 +2,15 @@
 # Control
 gdown 1YK-06Y8nYiw5oCarvMxfyBDMungkr--T
 gdown 1V9sSDxXK2q3p0hsXKhoSqvrMwhrpU2MN
+gdown 1fAQMQp9Unhl3DD3Crti2m6vzdIqt53cI
+gdown 1Sl3EGj3M_YEu1BFvtX5mTHAG6ejUA85Q
+gdown 1aSpp-r6m9zbyLZUJwe_K5QRdcHvtAJyU
+gdown 1OKIuYdj6b8ADlffbELWLkuoMyeqGa1vB
 
 # Treament
 gdown 1WAMsniml7QzJ1vkj4JnirzoHO5PJJUHy
 gdown 1q-ISLqiPVkvaIu9JLdKgQCUqyEutUhR-
+gdown 1Xpj0YUP2exl_BpPgE0WJfTsn5QSTWCDf
+gdown 1CQw6eJSk0qUvK7Pk2BGiFivwBCo49TUm
+gdown 1nSYq9DrDMKN8tW8yADz-KHn0HeFf7XZA
+gdown 1-8R39lWfXXO-uru9O-ws7RJ2JePTbq3z
